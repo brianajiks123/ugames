f6fdccf2c3f7a68ed4311b9b89a8004910596d0d
brianajiks123 <brianajiks123@gmail.com>
2026-01-31 15:56:07 +0700

    rename env

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,7 @@ import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    secret: process.env.AUTH_SECRET,
     providers: [
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
